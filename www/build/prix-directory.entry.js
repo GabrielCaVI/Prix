1aff99cebc4789d5f1c6b2ebb8ebb6277936dd6f
Arturo Salinas Ortiz <salor_arturo@hotmail.com>
2019-06-14 21:03:55 -0500

    Changed project boilerplate to component variant

---
+++ www/build/prix-directory.entry.js
@@ -0,0 +1,13 @@
+import { r as registerInstance } from './app-50de0c85.js';
+
+class Directory {
+    constructor(hostRef) {
+        registerInstance(this, hostRef);
+    }
+    render() {
+        return (h("div", null, h("h1", null, "testn")));
+    }
+    static get style() { return "body{\n  background-color: aqua;\n  font-family: \'Lucida Sans\', \'Lucida Sans Regular\', \'Lucida Grande\', \'Lucida Sans Unicode\', Geneva, Verdana, sans-serif;\n}"; }
+}
+
+export { Directory as prix_directory };
