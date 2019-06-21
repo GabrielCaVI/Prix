32fcaab9f75abc1f07598c5c6658aa3d73ae8c33
Arturo Salinas Ortiz <salor_arturo@hotmail.com>
2019-06-20 20:12:08 -0500

    Implemented filter fn

--- prix/src/components/Directory/directory.tsx
+++ prix/src/components/Directory/directory.tsx
@@ -31,18 +31,23 @@ export class Directory {
         return all;
     } */
     toggleSelection() {
+        var txt;
         let selectAllBtn = document.getElementById('select-all') as HTMLInputElement;
         let selectLabel = document.getElementById('select-all-label') as HTMLLabelElement;
         let letterItems = document.getElementsByClassName('checkInput') as HTMLCollectionOf<HTMLInputElement>;
+        let letterLabels = document.getElementsByClassName('checkLabel') as HTMLCollectionOf<HTMLLabelElement>;
         if (selectAllBtn.checked) {
             selectLabel.innerHTML = 'Unselect all';
             for (var i = 0; i < letterItems.length; i++) {
+                txt = letterLabels[i].textContent || letterLabels[i].innerText;
                 letterItems[i].checked = true;
+                this.filterByCheckbox(txt);
             }
         } else {
             selectLabel.innerHTML = 'All items';
-            for (var i = 0; i < letterItems.length; i++) {
+            for (var i = 0; i < letterItems.length; i++) {txt = letterLabels[i].textContent || letterLabels[i].innerText;
                 letterItems[i].checked = false;
+                this.filterByCheckbox(txt);
             }
         }
     }
@@ -51,30 +56,29 @@ export class Directory {
     }
 
     filterByInput(){
-        let input = document.getElementById("searchbox") as HTMLInputElement
-        let filterString = input.value.toUpperCase() as string;
-        let letters = document.getElementsByClassName("") as HTMLCollectionOf<HTML>
-        // Declare variables 
-        /* var input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("myInput");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("myTable");
-
-        // Loop through all table rows, and hide those who don't match the search query
-         for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+        var p;
+        let searchbox = document.getElementById("searchbox") as HTMLInputElement // the searchbox element
+        let filter = searchbox.value.toUpperCase() as string; // the typed filter
+        let items = document.getElementsByClassName("item") as HTMLCollectionOf<HTMLDivElement>; //To hide the complete <div class=item>
+        for(let i = 0; i < items.length; i++){ // iterate over labels to match input
+            p = items[i].getElementsByTagName("p")[0].textContent || items[i].getElementsByTagName("p")[0].innerText;
+            if(p.toUpperCase().indexOf(filter) > -1){
+                items[i].style.display = "none"
+                this.filterLinks();
+            }else{
+                items[i].style.display = "";
             }
-            } 
-        } */
+        }
     }
-    filterByCheckbox(){
-
+    filterByCheckbox(check){
+        var p;
+        let items = document.getElementsByClassName("item") as HTMLCollectionOf<HTMLDivElement>;
+        for(let i = 0; i < items.length; i++){
+            p = items[i].getElementsByTagName("p")[0].textContent || items[i].getElementsByTagName("p")[0].innerText;
+            if(p.toUpperCase().indexOf(check) > -1){
+                items[i].style.display = items[i].style.display===''? 'none': '';
+            }
+        }
     }
     
     init(){
@@ -87,135 +91,134 @@ export class Directory {
 
     render() {
         const items = this.internalItems;
+        const show = false;
         return (
             <span>
-                
                 <div class="container-lg">
                     <div class="topnav">
                         <button id="showGrid">ABC</button>
                         <input type="checkbox" class="active" id="select-all" onClick={() => this.toggleSelection()} />
                         <label class="active" htmlFor="select-all" id="select-all-label">All Items</label>
                         <div class="search-container">
-                            <input id="searchbox" type="text" placeholder="Search..." onKeyDown={() => this.filterByInput()} />
+                            <input id="searchbox" type="text" placeholder="Search..." onKeyUp={() => this.filterByInput()} />
                         </div>
-
                             <div id="letters">
-                            <prix-modal></prix-modal>
+                                {show?
+                                <prix-modal></prix-modal>:
+                                ""
+                                }
                                 <ul id="lettersList">
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="#" />
+                                        <input type="checkbox" class="checkInput" id="#" onChange={() => this.filterByCheckbox("#")}/>
                                         <label htmlFor="#" class="checkLabel">#</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="A" />
+                                        <input type="checkbox" class="checkInput" id="A" onChange={() => this.filterByCheckbox("A")}/>
                                         <label htmlFor="A" class="checkLabel">A</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="B" />
+                                        <input type="checkbox" class="checkInput" id="B" onChange={() => this.filterByCheckbox("B")}/>
                                         <label htmlFor="B" class="checkLabel">B</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="C" />
+                                        <input type="checkbox" class="checkInput" id="C" onChange={() => this.filterByCheckbox("C")}/>
                                         <label htmlFor="C" class="checkLabel">C</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="D" />
+                                        <input type="checkbox" class="checkInput" id="D" onChange={() => this.filterByCheckbox("D")}/>
                                         <label htmlFor="D" class="checkLabel">D</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="E" />
+                                        <input type="checkbox" class="checkInput" id="E" onChange={() => this.filterByCheckbox("E")}/>
                                         <label htmlFor="E" class="checkLabel">E</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="F" />
+                                        <input type="checkbox" class="checkInput" id="F" onChange={() => this.filterByCheckbox("F")}/>
                                         <label htmlFor="F" class="checkLabel">F</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="G" />
+                                        <input type="checkbox" class="checkInput" id="G" onChange={() => this.filterByCheckbox("G")}/>
                                         <label htmlFor="G" class="checkLabel">G</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="H" />
+                                        <input type="checkbox" class="checkInput" id="H" onChange={() => this.filterByCheckbox("H")}/>
                                         <label htmlFor="H" class="checkLabel">H</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="I" />
+                                        <input type="checkbox" class="checkInput" id="I" onChange={() => this.filterByCheckbox("I")}/>
                                         <label htmlFor="I" class="checkLabel">I</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="J" />
+                                        <input type="checkbox" class="checkInput" id="J" onChange={() => this.filterByCheckbox("J")}/>
                                         <label htmlFor="J" class="checkLabel">J</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="K" />
+                                        <input type="checkbox" class="checkInput" id="K" onChange={() => this.filterByCheckbox("K")}/>
                                         <label htmlFor="K" class="checkLabel">K</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="L" />
+                                        <input type="checkbox" class="checkInput" id="L" onChange={() => this.filterByCheckbox("L")}/>
                                         <label htmlFor="L" class="checkLabel">L</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="M" />
+                                        <input type="checkbox" class="checkInput" id="M" onChange={() => this.filterByCheckbox("M")}/>
                                         <label htmlFor="M" class="checkLabel">M</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="N" />
+                                        <input type="checkbox" class="checkInput" id="N" onChange={() => this.filterByCheckbox("N")}/>
                                         <label htmlFor="N" class="checkLabel">N</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="O" />
+                                        <input type="checkbox" class="checkInput" id="O" onChange={() => this.filterByCheckbox("O")}/>
                                         <label htmlFor="O" class="checkLabel">O</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="P" />
+                                        <input type="checkbox" class="checkInput" id="P" onChange={() => this.filterByCheckbox("P")}/>
                                         <label htmlFor="P" class="checkLabel">P</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="Q" />
+                                        <input type="checkbox" class="checkInput" id="Q" onChange={() => this.filterByCheckbox("Q")}/>
                                         <label htmlFor="Q" class="checkLabel">Q</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="R" />
+                                        <input type="checkbox" class="checkInput" id="R" onChange={() => this.filterByCheckbox("R")}/>
                                         <label htmlFor="R" class="checkLabel">R</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="S" />
+                                        <input type="checkbox" class="checkInput" id="S" onChange={() => this.filterByCheckbox("S")}/>
                                         <label htmlFor="S" class="checkLabel">S</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="T" />
+                                        <input type="checkbox" class="checkInput" id="T" onChange={() => this.filterByCheckbox("T")}/>
                                         <label htmlFor="T" class="checkLabel">T</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="U" />
+                                        <input type="checkbox" class="checkInput" id="U" onChange={() => this.filterByCheckbox("U")}/>
                                         <label htmlFor="U" class="checkLabel">U</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="V" />
+                                        <input type="checkbox" class="checkInput" id="V" onChange={() => this.filterByCheckbox("V")}/>
                                         <label htmlFor="V" class="checkLabel">V</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="W" />
+                                        <input type="checkbox" class="checkInput" id="W" onChange={() => this.filterByCheckbox("W")}/>
                                         <label htmlFor="W" class="checkLabel">W</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="X" />
+                                        <input type="checkbox" class="checkInput" id="X" onChange={() => this.filterByCheckbox("X")}/>
                                         <label htmlFor="X" class="checkLabel">X</label>
                                     </li>
                                     <li class="pagnLink">
-                                        <input type="checkbox" class="checkInput" id="Y" />
+                                        <input type="checkbox" class="checkInput" id="Y" onChange={() => this.filterByCheckbox("Y")}/>
                                         <label htmlFor="Y" class="checkLabel">Y</label>
                                     </li>
                                     <li class="pagnLink">
-
-                                        <input type="checkbox" class="checkInput" id="Z" />
+                                        <input type="checkbox" class="checkInput" id="Z" onChange={() => this.filterByCheckbox("Z")}/>
                                         <label htmlFor="Z" class="checkLabel">Z</label>
                                     </li>
-
                                 </ul>
                             </div>
                         </div>
-                    </div>
                     <div class="item-container">
                         {items.map((item) => (
                             <div class="item">
@@ -229,11 +232,11 @@ export class Directory {
                             </div>
                         ))}
                     </div>
-
+                </div>
             </span>
         )
     }
-    sortItems() {
+    private sortItems() {
         const { sort } = this.configuration;
         const { direction, byField } = sort;
         const { items } = this.data;
@@ -251,5 +254,8 @@ export class Directory {
                 );
             }
         }
+    }
+    private filterLinks(){
+
     }
 }
