3e0a1a3b2393260412c1d9a5a287bd5629bcbd5a
Arturo Salinas Ortiz <salor_arturo@hotmail.com>
2019-06-26 00:37:02 -0500

    Revert "merge test"

--- www/build/app.esm.js
+++
@@ -1,6 +0,0 @@
-import { p as patchBrowser, g as globals, b as bootstrapLazy } from './app-50de0c85.js';
-
-patchBrowser().then(resourcesUrl => {
-  globals();
-  return bootstrapLazy([["prix-directory",[[0,"prix-directory"]]]], { resourcesUrl });
-});
