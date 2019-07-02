c8ce3afe7b3c2c408eda261f58e5b048f987d61e
Victor Cruz <vcruzrey1@gmail.com>
2019-07-02 12:29:36 -0500

    Changes due to working on slots and css design

--- src/components/Modal/modal.tsx
+++ src/components/Modal/modal.tsx
@@ -4,51 +4,6 @@ import { Component, Prop, Method,h} from '@stencil/core'
     styleUrl: 'modal.scss'
 })
 export class modal {
-  /* GABO
-    @Element() el: HTMLElement;
-    @Prop() data?: any = {};
-    @Prop({ mutable: true }) configuration?: any = {};
-    @Prop() label: string;
-    @Prop({ mutable: true }) styling?: any = {};
-    @State() value: string = this.label;
-    ovelayOn() {
-        let boton = document.getElementById('prix-boton')
-        let cortina = document.getElementById('modal')
-        let msg = document.getElementById('prix-box')
-        boton.classList.toggle('overlay')
-        cortina.style.display= "block"
-        msg.style.display = 'block'
-        if (boton.classList.contains('overlay')){
-            boton.style.display="none"
-        }
-    }
-    overlayOff() {
-        let over = document.getElementById('modal')
-        let boton = document.getElementById('prix-boton')
-        let msg = document.getElementById('prix-box')
-        boton.classList.toggle('overlay')
-        if (!boton.classList.contains('overlay')){
-            boton.style.display="block"
-        }
-        over.style.display ="none"
-        msg.style.display="none"
-    }
-    render() {
-        return [
-            <div class="overlay" id="modal">
-                <span class="dismiss" onClick={() => this.overlayOff()}><i class="fa fa-window-close" aria-hidden="true"></i></span>
-                <div class ="prix-modal-box" id="prix-box">
-                    <div class="prix-modal-message">
-                        <p>{this.label}</p>
-                        <slot name="content"></slot>
-                        <button class="prix-modal-button" id="cancel" onClick={() => this.overlayOff()} >{this.canceLabel}</button> <button class="prix-modal-button" id="accept">{this.acceptLabel}</button>
-                    </div>
-                </div>
-            </div>,
-            <button class="prix-button" id="prix-boton" onClick={() => this.ovelayOn()}>{this.value}</button>
-        ]
-    }
-*/
 
   @Prop({ mutable: true }) mColorHeader: string = "";
   @Prop({ mutable: true }) mColorBody: string = "";
@@ -72,7 +27,7 @@ export class modal {
 
   componentDidLoad(){
     console.log(this.mColorHeader);
-    
+
     let modalBack = document.getElementById("myModal");
     modalBack.style.backgroundColor = this.dOpacityColor;
 
@@ -97,8 +52,9 @@ export class modal {
       <div>
         <div id ="myModal" class = "modal">
           <div id ="contentModal" class = "modal-content">
+          <div class="modal-content-2">
             <div id ="headerModal" class = "modal-header">
-              <slot name="headerModalContent"></slot>
+              <slot id="headSlot" name="headerModalContent"></slot>
             </div>
             <div id ="bodyModal" class = "modal-body">
               <slot name="bodyModalContent"></slot>
@@ -108,8 +64,55 @@ export class modal {
               <button class="button" onClick={() => this.hideModal()}>cerrar</button>
             </div>
           </div>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+/* GABO
+  @Element() el: HTMLElement;
+  @Prop() data?: any = {};
+  @Prop({ mutable: true }) configuration?: any = {};
+  @Prop() label: string;
+  @Prop({ mutable: true }) styling?: any = {};
+  @State() value: string = this.label;
+  ovelayOn() {
+      let boton = document.getElementById('prix-boton')
+      let cortina = document.getElementById('modal')
+      let msg = document.getElementById('prix-box')
+      boton.classList.toggle('overlay')
+      cortina.style.display= "block"
+      msg.style.display = 'block'
+      if (boton.classList.contains('overlay')){
+          boton.style.display="none"
+      }
+  }
+  overlayOff() {
+      let over = document.getElementById('modal')
+      let boton = document.getElementById('prix-boton')
+      let msg = document.getElementById('prix-box')
+      boton.classList.toggle('overlay')
+      if (!boton.classList.contains('overlay')){
+          boton.style.display="block"
+      }
+      over.style.display ="none"
+      msg.style.display="none"
+  }
+  render() {
+      return [
+          <div class="overlay" id="modal">
+              <span class="dismiss" onClick={() => this.overlayOff()}><i class="fa fa-window-close" aria-hidden="true"></i></span>
+              <div class ="prix-modal-box" id="prix-box">
+                  <div class="prix-modal-message">
+                      <p>{this.label}</p>
+                      <slot name="content"></slot>
+                      <button class="prix-modal-button" id="cancel" onClick={() => this.overlayOff()} >{this.canceLabel}</button> <button class="prix-modal-button" id="accept">{this.acceptLabel}</button>
+                  </div>
+              </div>
+          </div>,
+          <button class="prix-button" id="prix-boton" onClick={() => this.ovelayOn()}>{this.value}</button>
+      ]
+  }
+*/
